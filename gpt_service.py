e65d2ca2a34bf6e23d3107262432ff99f021d012
ganggyunggyu <qwzx16@naver.com>
2025-08-08 17:42:52 +0900

    fix

--- gpt_service.py
+++ gpt_service.py
@@ -1,6 +1,17 @@
+
+from  pymongo import MongoClient
+from config import MONGODB_URI
 from config import openai_client
 from prompts import getKoPrompt, ref
+def get_db(db_name, col_name):
+    client = MongoClient(MONGODB_URI)
+    db = client[db_name]
+    word_collection = db[col_name]    
+    return word_collection
 
+col = get_db(db_name='hospital', col_name='word')
+
+docs = list(col.find())
 def send_prompt_to_gpt(keyword):
     KO_PROMPT = getKoPrompt(keyword)
     
@@ -8,14 +19,20 @@ def send_prompt_to_gpt(keyword):
         response = openai_client.chat.completions.create(
             model='gpt-4.1-2025-04-14',
             messages=[
-                {"role": "system", "content": KO_PROMPT},
+                {"role": "system", "content": ' You are a professional blog post writer. Your task is to generate a blog post based on provided analysis data and user instructions.'},
                 {"role": "user", "content": f"""
+                [사용자 요구사항]
                  
-                 3000단어 이상 필수
-                 ---
-                 키워드: {keyword} 
-                 ---
-                 참고 문서: {ref}
+                {KO_PROMPT}
+                
+                [키워드] [참고 문서] [추가 요청사항]
+
+                {keyword}
+
+                {ref}                
+
+                [참고 단어]
+                {col}
 """}
             ],
             temperature=0.0,
--- main.py
+++ main.py
@@ -64,4 +64,10 @@ def generate_text(request: GenerationRequest):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host=True, port=8000)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=3000,
+        # ssl_keyfile="key.pem",
+        # ssl_certfile="cert.pem"
+    )
